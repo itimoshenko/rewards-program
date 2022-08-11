783dd219dbdd3c9919ed96f3d5456b9eaf777fbc
Ivan Timoshenko <tis1993@mail.ru>
2022-08-11 23:47:50 +0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+import packageDescription from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(packageDescription.name)
+    .setDescription(packageDescription.description)
+    .setVersion(packageDescription.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.SERVER_PORT);
 }
+
 bootstrap();
